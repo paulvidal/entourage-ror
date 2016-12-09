b49769d472648f11c8f1659e0e60a2a4cc11a7a6
Chhim Stéphanie <stephaniechhim@gmail.com>
2016-12-09 19:21:59 +0100

    User Factory  > removed device_type and device_id attributes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,10 +7,6 @@ FactoryGirl.define do
     sequence :email do |n|
       "user#{n}@mail.com"
     end
-    device_type :android
-    sequence :device_id do |n|
-      "device id #{n}"
-    end
     sequence :phone do |n|
       "+336%08i" % n
     end
