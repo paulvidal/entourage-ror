9aa4ccd18d0572921ed0d1b6e0d7dd7b3f9f5583
Grégoire Clermont <gregoire@entourage.social>
2018-01-12 19:23:36 +0100

    perf(feed): avoid unnecessary n+1 SQL request in partner serialization

--- app/serializers/v1/partner_serializer.rb
+++ app/serializers/v1/partner_serializer.rb
@@ -7,7 +7,12 @@ module V1
                :default
 
     def default
-      scope[:user].partners.include?(object)
+      # perf optimization for the feed: prevent n+1 request
+      if scope[:user].default_user_partners.loaded?
+        scope[:user].default_user_partners.any? { |up| up.partner_id == object.id }
+      else
+        scope[:user].partners.include?(object)
+      end
     end
   end
 end
